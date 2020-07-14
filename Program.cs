6a92ed62fb9057a439880e841297fcde926e9b34
IROEGBU! <iroegbu@IROEGBUs-MacBook-Pro.local>
2020-07-14 15:04:14 +0100

    Feature (Command): Command Parser
    Includes command parser and CLI framework (CliFx) and basic options and flags with default values

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+
+namespace envars
+{
+  class Program
+  {
+    public static async Task<int> Main() =>
+    await new CliApplicationBuilder()
+        .AddCommandsFromThisAssembly()
+        .Build()
+        .RunAsync();
+  }
+
+  [Command]
+  public class Fetch : ICommand
+  {
+    [CommandOption("source", 's', Description = "Source of secrets (awssm, local). \n \t\t\t awssm: fetches variables from AWS Secrets Manager \n \t\t\t local: loads variables from local .env file. \n")]
+    public string Source { get; set; } = "local";
+
+    [CommandOption("config", 'c', Description = "Config file containing required configurations to load")]
+    public string ConfigFile { get; set; } = "";
+
+    public ValueTask ExecuteAsync(IConsole console)
+    {
+      console.Output.WriteLine($"Source of environment variables is {Source}");
+
+      return default;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mamba
+# envars
