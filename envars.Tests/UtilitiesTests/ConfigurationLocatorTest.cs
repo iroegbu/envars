abf18e49b49719b285d5c583b633ef07d8b64b1f
IROEGBU! <iiroegbu@hotmail.co.uk>
2020-09-01 12:49:06 +0100

    Feature(Import): Import local environment (#14)
    * Feature(Import): Import local environment
    
    Import local environment variables and load in to system process.
    
    Resolves #4
    
    * Fix(Test): Fix tests
    
    Fixes issues with testing for Windows environment. Building paths and changing linux vs windows slashes.
    
    * Fixing slash issue to support windows.

---
+++ envars.Tests/UtilitiesTests/ConfigurationLocatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using envars.Utilities;
+using Xunit;
+using System.IO;
+
+namespace envars.Tests.UtilitiesTests
+{
+    public class ConfigurationLocatorTest
+    {
+        public ConfigurationLocatorTest()
+        {
+        }
+
+        [Theory]
+        [InlineData(".")]
+        public void LocateConfigFileValue_ShouldPass(string ConfigPath)
+        {
+            var expected = Path.Combine(".", ".envars");
+            var result = ConfigurationLocator.LocateConfigFile(ConfigPath);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("etc")]
+        public void LocateConfigFileEmpty_ShouldPass(string ConfigPath)
+        {
+            var expected = Path.Combine("etc", ".envars");
+            var result = ConfigurationLocator.LocateConfigFile(ConfigPath);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ envars/Utilities/ConfigurationLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace envars.Utilities
+{
+    public static class ConfigurationLocator
+    {
+        private static string[] _configPaths = { AppDomain.CurrentDomain.BaseDirectory, "/etc", "/$HOME" };
+
+        public static string LocateConfigFile(string ConfigPath)
+        {
+            if (ConfigPath != "")
+            {
+                return Path.Combine(ConfigPath, ".envars");
+            }
+            return FindConfigFile();
+        }
+
+        public static string FindConfigFile()
+        {
+            foreach (var dirPath in _configPaths)
+            {
+                if (File.Exists(Path.Combine(dirPath, ".envars")))
+                {
+                    return Path.Combine(dirPath, ".envars");
+                }
+            }
+            throw new FileNotFoundException();
+        }
+    }
+}
